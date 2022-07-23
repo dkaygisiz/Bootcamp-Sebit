0f4df6914f9e37f438cf4430c61a15316b2e06c1
Doğukan Kaygısız <kemaldogukankaygisiz@gmail.com>
2022-07-23 18:39:22 +0300

    Add files via upload
    23.07.2022 Notlar

---
+++ Bootcamp Sebit 23.07.2022/9-Javascript/project.js
@@ -0,0 +1,37 @@
+/*
+let gun = 12;
+let ay = 9;
+let yıl = 1997;
+
+gun = 15;
+
+console.log(gun, ay, yıl);
+console.log(
+  "Ahmet Kerem Kantarcı " + gun + "/" + ay + "/" + yıl + " tarihinde doğmuştur"
+);
+
+const ikinciElAraba = 200000;
+console.log(ikinciElAraba);
+
+var a = 5;
+
+var a = 3;
+console.log(a);
+
+var c = 5;
+console.log("C değeri dışarıda", c);
+{
+  var c = 3;
+  console.log("C değeri içeride", c);
+}
+console.log("C değeri dışarıda", c);
+
+let d = 5;
+console.log("D değeri dışarıda", d);
+{
+  let d = 3;
+  console.log("D değeri içeride", d);
+}
+console.log("D değeri dışarıda", d);
+
+*/
